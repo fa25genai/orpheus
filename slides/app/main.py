9eff5ba6ee754282adc05e2c1ee90c4594f89f40
Kilian Krampf <kilian@krampf.de>
2025-09-22 15:19:34 +0200

    Basic FastAPI + langchain project sceleton

---
+++ slides/app/main.py
@@ -0,0 +1,9 @@
+import typing
+
+from fastapi import FastAPI
+
+app = FastAPI(title="orpheus-service-slides")
+
+@app.get("/")
+async def root() -> typing.Any:
+    return {"message": "Hello, AI world!"}
