8f8a36b36f74fa46eb310164dcfa4a32f3165c2e
Kilian Krampf <kilian@krampf.de>
2025-09-22 18:15:49 +0200

    Add global executor service

--- slides/src/service_slides/main.py
+++ slides/src/service_slides/main.py
@@ -1,5 +1,7 @@
 import os
 import typing
+from concurrent.futures.thread import ThreadPoolExecutor
+from os import cpu_count
 
 from fastapi import FastAPI
 from langchain.chat_models import init_chat_model
@@ -10,13 +12,17 @@ async def lifespan(app: FastAPI):
     model = None
     if "GOOGLE_API_KEY" in os.environ:
         model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+    app.state.executor = ThreadPoolExecutor(max_workers=cpu_count())
 
     if model is not None:
         app.state.model = model
     else:
         raise RuntimeError("No supported LLM API key supplied")
+
     yield
+
     # Teardown code goes here
+    app.state.executor.shutdown()
 
 
 app = FastAPI(title="orpheus-service-slides", lifespan=lifespan)
