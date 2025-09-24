e43235932f07aaedbae833c11933bea4801b89f7
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2025-09-24 16:28:14 +0200

    Added CORS

--- core/src/service_core/main.py
+++ core/src/service_core/main.py
@@ -3,6 +3,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .apis.core_api import router as CoreApiRouter
+from fastapi.middleware.cors import CORSMiddleware
 
 # @asynccontextmanager
 # async def lifespan(app: FastAPI):
@@ -20,6 +21,16 @@ app = FastAPI(
     description="Customized API for Orpheus core orchestration.",
     version="0.1.0",
     # lifespan=lifespan
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(CoreApiRouter)
