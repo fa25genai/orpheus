bdccf184833039f5c4c96d03261b2a9bb9f0bac6
Eslam Nasrallah <eslam.nasrallah@tum.de>
2025-09-25 14:55:10 +0200

    add slides api to client handler

--- core/src/service_core/services/client_handler.py
+++ core/src/service_core/services/client_handler.py
@@ -1,13 +1,14 @@
 import httpx
 from service_core.services import decompose_input, fetch_mock_data, script_generation, narration_generation
+from service_core.services.fetch_mock_data import create_context_mock
 
 client = httpx.AsyncClient()
 
 DI_API_URL = "http://docint:25565"
-SLIDES_API_URL = "https://videos:8050"
+SLIDES_API_URL = "https://slides:30606"
 
 
-async def process_prompt(prompt_id: str, prompt_request: str):
+async def process_prompt(prompt_id: str, prompt_request: str): # TODO: type prompt_request
     """
     Asynchronously calls the appropriate external API to get the final result.
     """
@@ -43,7 +44,15 @@ async def process_prompt(prompt_id: str, prompt_request: str):
     #await asyncio.sleep(5)
     refined_output = script_generation.generate_script(str(fetch_mock_data.create_demoretrieved_content()), fetch_mock_data.create_demo_user())
     #print(refined_output)
-    #lecture_script = refined_output.get("lectureScript", "")
+    lecture_script = refined_output.get("lectureScript", "")
+
+    context_mock = create_context_mock(prompt_request.courseId, prompt_id, lecture_script, fetch_mock_data.create_demo_user())
+    slides_response = await client.post(
+            f"{SLIDES_API_URL}/v1/slides/generate",
+            json=context_mock,
+            timeout=300.0,
+        )
+    print("Slides API response:", slides_response.json())
     #print("\n\nGenerated Lecture Script:", lecture_script)
     #example_slides = fetch_mock_data.create_demo_slides()
     #print("\nExample Slides:", example_slides)
