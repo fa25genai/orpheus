8ac9f0566eea6db39f9f4c59e6021efb9d7d1936
Kilian Krampf <kilian@krampf.de>
2025-09-26 22:47:30 +0200

    Status: Bugfix: Fix websocket handling

--- status/src/service_status/apis/status_api.py
+++ status/src/service_status/apis/status_api.py
@@ -105,7 +105,7 @@ async def websocket_status(
 
     async def send_status_update(status: Status):
         try:
-            await websocket.send_json(status)
+            await websocket.send_text(status.to_json())
         except WebSocketDisconnect:
             status_manager.remove_listener(promptId, id)
 
