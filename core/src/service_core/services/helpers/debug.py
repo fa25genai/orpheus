90bad4050d6830aabf95d4e741da86f1e735f24d
Lukas <st187218@stud.uni-stuttgart.de>
2025-09-28 17:08:01 +0200

    Update debug.py

--- core/src/service_core/services/helpers/debug.py
+++ core/src/service_core/services/helpers/debug.py
@@ -1,19 +1,4 @@
 import datetime
 
-# Global flag to control debug printing
-DEBUG_ENABLED: bool = True
-
-
-def enable_debug() -> None:
-    global DEBUG_ENABLED
-    DEBUG_ENABLED = True
-
-
-def disable_debug() -> None:
-    global DEBUG_ENABLED
-    DEBUG_ENABLED = False
-
-
 def debug_print(message: str) -> None:
-    if DEBUG_ENABLED:
-        print(f"[DEBUG {datetime.datetime.now().isoformat()}]: {message}")
+    print(f"[DEBUG {datetime.datetime.now().isoformat()}]: {message}")
