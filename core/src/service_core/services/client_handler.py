183ceab24439cf2dfb0543f28cec9dfe18d34f24
Florian Glombik <florian.glombik@tum.de>
2025-09-28 14:57:12 +0200

    Add urls for local development

--- core/src/service_core/services/client_handler.py
+++ core/src/service_core/services/client_handler.py
@@ -25,6 +25,13 @@ DI_API_URL = "http://docint:25565"
 SLIDES_API_URL = "http://slides:30606"
 AVATAR_API_URL = "http://avatar-video-producer:9000"
 STATUS_API_URL = "http://status-service:19910"
+
+# Use this when you start the service locally outside of a docker container
+# DI_API_URL = "http://localhost:25565"
+# SLIDES_API_URL = "http://localhost:30606"
+# AVATAR_API_URL = "http://localhost:9000"
+# STATUS_API_URL = "http://localhost:19910"
+
 DEBUG = int(os.environ.get("ORPHEUS_DEBUG", "1"))
 
 async def update_status(prompt_id: str, patch: StatusPatch, client: httpx.AsyncClient) -> None:
