590901eb5ff41720845013b8857fc5f79fad7302
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2025-09-28 14:37:15 +0200

    Added debug flag

--- core/src/service_core/services/client_handler.py
+++ core/src/service_core/services/client_handler.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Union
 
 import httpx
 from dotenv import load_dotenv
+from pathlib import Path
 
 import service_core.services.fetch_mock_data as mock_service
 from service_core.impl.tracker import tracker
@@ -19,13 +20,15 @@ from service_core.services.user_summary import summarize_content_with_llama
 from service_status.models.status_patch import StatusPatch
 from service_status.models.step_status import StepStatus
 
-load_dotenv()
+PROJECT_ROOT = Path(__name__).resolve().parent.parent
+DOTENV_PATH = PROJECT_ROOT / '.env'
+load_dotenv(dotenv_path=DOTENV_PATH)
 
 DI_API_URL = "http://docint:25565"
 SLIDES_API_URL = "http://slides:30606"
 AVATAR_API_URL = "http://avatar-video-producer:9000"
 STATUS_API_URL = "http://status-service:19910"
-DEBUG = int(os.environ.get("ORPHEUS_DEBUG", "1"))
+DEBUG = int(os.getenv("ORPHEUS_DEBUG", "2"))
 
 async def update_status(prompt_id: str, patch: StatusPatch, client: httpx.AsyncClient) -> None:
     print(f"Updating status for {prompt_id} with patch: {patch.to_json()}", flush=True)
