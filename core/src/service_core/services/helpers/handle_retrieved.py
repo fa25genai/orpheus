89e7c159668951384b5a2caac20985c86f2544d6
Florian Glombik <florian.glombik@tum.de>
2025-09-28 11:40:19 +0200

    Use actual data instead of mock

--- core/src/service_core/services/helpers/handle_retrieved.py
+++ core/src/service_core/services/helpers/handle_retrieved.py
@@ -32,7 +32,7 @@ def convert_json_structure(content: List[Dict[str, Any]]) -> List[Dict[str, Any]
     Converts the input JSON to include an 'assets' key with image data.
     """
     output_data = []
-    for item in retrieved_content:
+    for item in content:
         new_item = {"content": item["content"], "assets": []}
 
         if "images" in item and item["images"]:
@@ -62,7 +62,7 @@ def convert_json_structure(content: List[Dict[str, Any]]) -> List[Dict[str, Any]
 
 
 # --- Input Data ---
-retrieved_content: List[Dict[str, Any]] = [
+retrieved_content_mock: List[Dict[str, Any]] = [
     {
         "content": [
             "A for loop is a control flow statement for specifying iteration, which allows code to be executed repeatedly.",
@@ -90,6 +90,6 @@ retrieved_content: List[Dict[str, Any]] = [
 ]
 
 if __name__ == "__main__":
-    a = convert_json_structure(retrieved_content)
+    a = convert_json_structure(retrieved_content_mock)
     print("Conversion completed.")
     print(json.dumps(a, indent=4))
