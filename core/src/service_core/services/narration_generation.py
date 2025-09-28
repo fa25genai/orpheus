965e195decf7f49d048cf0aa5208c024e939fa38
Lukas <st187218@stud.uni-stuttgart.de>
2025-09-28 17:26:58 +0200

    Update narration_generation.py

--- core/src/service_core/services/narration_generation.py
+++ core/src/service_core/services/narration_generation.py
@@ -16,7 +16,7 @@ import json
 from typing import Any, Dict
 
 from service_core.models.user_profile import UserProfile
-from service_core.services.helpers.debug import debug_print, enable_debug
+from service_core.services.helpers.debug import debug_print
 from service_core.services.helpers.llm import ask_llm
 from service_core.services.helpers.loaders import load_prompt
 
