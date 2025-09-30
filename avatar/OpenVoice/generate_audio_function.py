99fb7fae163ae7f3453a4ef6816c0a55ef968ff7
Ali Bagherzadeh <88430277+GAlibrz@users.noreply.github.com>
2025-09-30 16:11:08 +0200

    Fix uvicorn app reference in main

--- avatar/OpenVoice/generate_audio_function.py
+++ avatar/OpenVoice/generate_audio_function.py
@@ -494,7 +494,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "generate_audio_function:app",
+        "app:app",
         host="0.0.0.0",
         port=8000,
         reload=True
