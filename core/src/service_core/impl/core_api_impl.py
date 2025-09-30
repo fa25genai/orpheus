d335162d239f2fa684e6abfa5278becb45906a96
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2025-09-30 16:02:03 +0200

    Add default uuid

--- core/src/service_core/impl/core_api_impl.py
+++ core/src/service_core/impl/core_api_impl.py
@@ -34,6 +34,10 @@ class CoreApiImpl(BaseCoreApi):  # type: ignore[no-untyped-call]
             #     executor.submit(process_prompt_handler, prompt_id, prompt_request)
             #     print(prompt_id)
             executor = get_executor(prompt_id)
+            if prompt_request.user_persona is None:
+                raise ValueError("User persona must be defined for prompt requests from client.")
+
+            prompt_request.user_persona.id = uuid4()    # Remove later when client provides ID
             tracker.log(f"Initializing CoreThreadPoolExecutor for {prompt_id}")
             executor.submit(process_prompt_handler, prompt_id, prompt_request)
 
