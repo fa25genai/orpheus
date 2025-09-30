0d79cb3b9d4b1e8a32d85a81c73cac5404106a2f
Kilian Krampf <kilian@krampf.de>
2025-09-30 23:15:25 +0200

    Status: Bugfix Typo

--- core/src/service_core/services/client_handler.py
+++ core/src/service_core/services/client_handler.py
@@ -37,10 +37,10 @@ AVATAR_API_URL = "http://avatar-video-producer:9000"
 STATUS_API_URL = "http://status-service:19910"
 
 # Use this when you start the service locally outside a docker container
-DI_API_URL = "http://localhost:25565"
-SLIDES_API_URL = "http://localhost:30606"
-AVATAR_API_URL = "http://localhost:9000"
-STATUS_API_URL = "http://localhost:19910"
+# DI_API_URL = "http://localhost:25565"
+# SLIDES_API_URL = "http://localhost:30606"
+# AVATAR_API_URL = "http://localhost:9000"
+# STATUS_API_URL = "http://localhost:19910"
 
 DEBUG = int(os.getenv("ORPHEUS_DEBUG", "0"))  # DEBUG enabled by default
 
@@ -162,7 +162,7 @@ async def generate_script(
         tracker.log("Generating script")
         await update_status(
             prompt_id,
-            StatusPatch(stepLectureScriptGeneration=StepStatus.IN_PROGRESS),
+            StatusPatch(stepAudioScriptGeneration=StepStatus.IN_PROGRESS),
             client,
         )
 
@@ -183,7 +183,7 @@ async def generate_script(
             retrieved_content, prompt_request.user_persona
         )
         await update_status(
-            prompt_id, StatusPatch(stepLectureScriptGeneration=StepStatus.DONE), client
+            prompt_id, StatusPatch(stepAudioScriptGeneration=StepStatus.DONE), client
         )
     except Exception as exception:
         logger.error(
@@ -191,7 +191,7 @@ async def generate_script(
         )
         await update_status(
             prompt_id,
-            StatusPatch(stepLectureScriptGeneration=StepStatus.FAILED),
+            StatusPatch(stepAudioScriptGeneration=StepStatus.FAILED),
             client,
         )
         raise exception
