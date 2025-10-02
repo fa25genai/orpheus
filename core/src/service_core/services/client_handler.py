76f3fb0a52ef33e7812431770059af2ac277682f
Florian Glombik <florian.glombik@tum.de>
2025-10-02 11:32:25 +0200

    add error log

--- core/src/service_core/services/client_handler.py
+++ core/src/service_core/services/client_handler.py
@@ -86,6 +86,7 @@ async def retrieve_subqueries_from_prompt(
         )
         return subqueries
     except Exception as exception:
+        logger.error(f'Failed to retrieve subqueries for prompt `{prompt_id}`', exc_info=exception)
         await update_status(
             prompt_id, StatusPatch(stepUnderstanding=StepStatus.FAILED), client
         )
